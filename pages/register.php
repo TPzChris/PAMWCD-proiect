c200f812c9c8d8d46c7293adf22273b49d1395a1
TPzChris <chris.ionut@gmail.com>
2022-05-14 00:52:01 +0300

    First commit

---
+++ pages/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="/css/register.css" />
+</head>
+<body>
+    <div class="loginbox">
+        <img src="/../images/register/register.png" class="avatar">
+        <h1>Register</h1>
+        <form>
+            <p>Username</p>
+            <input type="text" name="" placeholder="Introduceti username-ul...">
+            <p>Parola</p>
+            <input type="password" name="" placeholder="Introduceti parola..."><br>
+            <input type="password" name="" placeholder="Introduceti din nou parola..."><br>
+            <input type="submit" name="" value="register"><br>
+            <a href="/pages/login.php">Aveti cont?</a>
+        </form>
+    </div>
+</body>
+</html>
