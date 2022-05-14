066c8e06b51474ee4bedbbd90bdc6b75ebe0c53c
TPzChris <chris.ionut@gmail.com>
2022-05-14 03:00:00 +0300

    Login done

--- pages/login.php
+++ pages/login.php
@@ -12,12 +12,12 @@
     <div class="loginbox">
         <img src="/../images/login/avatar.png" class="avatar">
         <h1>Login</h1>
-        <form action="#" onsubmit="return formNotFilled(this);" method="post">
+        <form action="./../php/loginPHP.php" onsubmit="return formNotFilled(this);" method="post">
             <p>Username</p>
             <input type="text" name="username" id="username" placeholder="Introduceti username-ul...">
             <p>Parola</p>
             <input type="password" name="password" id="password" placeholder="Introduceti parola..."><br>
-            <input type="submit" name="login" id="login" value="Login" ><br>
+            <input type="submit" name="Login" id="Login" value="Login" ><br>
             <a href="/pages/register.php">Nu aveti un cont?</a>
         </form>
     </div>
